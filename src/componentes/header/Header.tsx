46d1f659b8ede02e881ca1c28ed561f8872f4139
LUANA DA SILVA TRINDADE <06997466125@ifms.edu.br>
2023-08-15 07:33:42 -0400

    primeiro commit

---
+++ src/componentes/header/Header.tsx
@@ -0,0 +1,33 @@
+import './Header.css'
+function Header(){
+    return(
+        <header>
+          <div>
+            logo
+          </div>
+        <nav>
+          <ul>
+            <li>
+              <li>
+                <a href="#">HOME</a>
+              </li>
+
+              <li>
+                <a href="#">SOBRE</a>
+              </li>
+
+              <li>
+                <a href="#">NOTICIAS</a>
+              </li>
+
+              <li>
+                <a href="#">CONTATO</a>
+              </li>
+
+             </li>
+            </ul>
+          </nav>
+        </header>
+    )
+}
+export default Header
