4bb4f4bdde8787d87817560741c86d0c09103d81
996075561 <luana.trindade@estudante.ifms.edu.br>
2023-08-22 08:05:54 -0400

    Iniciando a programção do footer

---
+++ src/componentes/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import './Footer.css'
+import Logo from '../../assets/pngfind.com-barbie-logo-png-667657.png'
+function Footer (){
+    return(
+        <footer>
+            
+             <img src={Logo} alt=""/>     
+    
+       </footer>
+    )
+}
+export default Footer
